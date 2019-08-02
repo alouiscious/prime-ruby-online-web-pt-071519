3f3017ef36b69d33ba3caf7399182073b3d72c0d
Renee Sims <info@reneesoul.com>
2019-08-02 18:23:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+  # determine the limit of the aurguments prime. Create an array of the divisible used to determine prime numbers. Divide each int passed in by each the divisibles. Return a boolean if the int is prime
+  
+
+def prime?(num)
+  is_prime = []
+  divis_array = (2...num)
+  
+ divis_array.each do |prime_test, is_prime|
+     if num % prime_test == 0 && num < 1
+       return false
+   elsif num >= 2
+      num % prime_test
+    end
+  end
+       is_prime << num
+
+end
